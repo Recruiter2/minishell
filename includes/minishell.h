a315e9883f7053b3de3ee243c5a28d27bddce2ee
Tim <timour1712@gmail.com>
2026-06-04 00:47:21 +0200

    added minishell . h lines however gives error if stdio is not included directly in main...

---
+++ includes/minishell.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tzinaliy <tzinaliy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/04 00:29:04 by tzinaliy          #+#    #+#             */
+/*   Updated: 2026/06/04 00:29:06 by tzinaliy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../Libft/libft.h"
