175931bfbb958d84312ba523fd46cfa9f51001ac
Tim <timour1712@gmail.com>
2026-06-04 13:26:49 +0200

    moved user input function into their file

--- src/user_input.c
+++ src/user_input.c
@@ -6,7 +6,42 @@
 /*   By: tzinaliy <tzinaliy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 14:55:51 by tzinaliy          #+#    #+#             */
-/*   Updated: 2026/06/03 17:49:17 by tzinaliy         ###   ########.fr       */
+/*   Updated: 2026/06/04 13:18:52 by tzinaliy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/minishell.h"
+
+// return 1 if line is all whitespace or empty
+int is_blank(const char *s)
+{
+	while (*s)
+	{
+		if (!ft_isspace((unsigned char)*s))
+			return 0;
+		s++;
+	}
+	return 1;
+}
+
+void	handle_input(void)
+{
+	char *line;
+	int all_blank;
+
+	while (1) {
+		line = readline("minishell$ ");
+		all_blank = is_blank(line);
+		if (!all_blank) {
+			add_shell_history(line);
+			if (strcmp(line, "history") == 0)
+				builtin_history();
+		}
+		if (!line)
+		{
+			write(1, "\n", 1);
+			break;
+		}
+		free(line);
+	}
+}
