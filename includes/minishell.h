4a30b7505f9c2407acdfb07fb7ced24e720b3f67
Tim <timour1712@gmail.com>
2026-06-04 13:20:07 +0200

    add function prototypes into header;

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tzinaliy <tzinaliy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 00:29:04 by tzinaliy          #+#    #+#             */
-/*   Updated: 2026/06/04 00:29:06 by tzinaliy         ###   ########.fr       */
+/*   Updated: 2026/06/04 13:19:27 by tzinaliy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,3 +17,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../Libft/libft.h"
+
+//function for user input or parsing...
+// this function check if readline is empty
+int is_blank(const char *s);
+void	handle_input(void);
+
+// history functions
+void add_shell_history(const char *line);
+void builtin_history(void);
