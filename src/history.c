93a9ddf786e425745969b5ec385a722ffa479f12
Tim <timour1712@gmail.com>
2026-06-04 13:25:41 +0200

    move history function from main into their folder

--- src/history.c
+++ src/history.c
@@ -6,7 +6,34 @@
 /*   By: tzinaliy <tzinaliy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 14:55:56 by tzinaliy          #+#    #+#             */
-/*   Updated: 2026/06/03 17:49:17 by tzinaliy         ###   ########.fr       */
+/*   Updated: 2026/06/04 13:16:16 by tzinaliy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/minishell.h"
+
+void add_shell_history(const char *line)
+{
+	if (!line || *line == '\0')
+		return;
+	add_history(line);
+
+}
+
+// Print history builtin: "history"
+// start index at 1 to match bash
+void builtin_history(void)
+{
+	int i;
+
+	i = 0;
+	HIST_ENTRY **hist_list = history_list();
+	if (!hist_list) {
+		return;
+	}
+	while ( hist_list[i] != NULL)
+	{
+		printf("%4d  %s\n", i + history_base, hist_list[i]->line);
+		i++;
+	}
+}
