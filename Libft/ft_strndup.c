8610d7ff8bbb978d1e9830bf7eeaf1d4d8f9611d
Tim <timour1712@gmail.com>
2026-06-08 14:47:35 +0200

    no update

--- Libft/ft_strndup.c
+++ Libft/ft_strndup.c
@@ -6,13 +6,13 @@
 /*   By: tzinaliy <tzinaliy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 14:42:26 by tzinaliy          #+#    #+#             */
-/*   Updated: 2026/06/08 14:43:10 by tzinaliy         ###   ########.fr       */
+/*   Updated: 2026/06/08 14:47:04 by tzinaliy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strndup(const char *s, size_t n)
+char	*ft_strndup(const char *s, size_t n)
 {
 	char *p = malloc(n + 1);
 	if (!p) 
