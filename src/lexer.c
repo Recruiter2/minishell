ea6f53706fc1f75d8a948494b6cc802939673c93
Recruiter <null@null.nul>
2026-06-11 21:18:29 +0200

    fix to requirements

--- src/lexer.c
+++ src/lexer.c
@@ -139,12 +139,8 @@ token_t *lexer(const char *str)
 			}
 			if (str[i] == '>' && str[i+1] == '>')
 			{
-				token_t *t = tok_new(T_REDIR_APPEND, NULL, 0);
-				if (!t)
-					goto fail;
-				if (!head)
-					head = t;
-				tail = append_token(tail, token);
+				if (!push_op(&head, &tail, T_REDIR_APPEND))
+					return (free_tokens_list(head), NULL);
 				i += 2;
 				continue;
 			}
