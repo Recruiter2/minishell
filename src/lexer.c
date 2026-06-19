2566fac39906ae57a7f4efa7e2d547be5e0e1b9f
Tim <timour1712@gmail.com>
2026-06-19 14:17:07 +0200

    fix function break down into smaller

--- src/lexer.c
+++ src/lexer.c
@@ -355,7 +355,7 @@ token_t	*lexer(const char *str)
 		// take substring start..i-1
 		txt = ft_strndup(str + start, i - start);
 		if (!txt)
-			goto fail;
+			return (free_tokens_list(head), NULL);
 		token = tok_new(T_WORD, txt, 0);
 		if (!token)
 		{
