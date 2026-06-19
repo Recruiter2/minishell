6300ab0fd588084da924d70156248c59e7bc4e89
Tim <timour1712@gmail.com>
2026-06-19 12:51:14 +0200

    transform or refactor from big into multiple simpler functions; update syntax to requirement comment old

--- src/lexer.c
+++ src/lexer.c
@@ -111,15 +111,202 @@ static int	redirect_choice(t_token	*head, t_token	*tail, char c)
 	return (push_op(&head, &tail, res));
 }
 
-//the whole point of lexer is to give the type of the input and for now just remove spaces
-//but by return type we already can see it return a linked list of tokens
-token_t *lexer(const char *str)
+// the whole point of this function is to determine if the current str[i]
+// is a pipe heredoc redir append, redir in, redir out
+// then it adds to our token res
+// this way we build our tokens echo "str" | wc... or rather their types
+// what is tail for probably to know the end...
+// head is needed for check it exist
+// tail is used to add in the end
+int	pipe_less_more_(const char *str, int *i, t_token	**head, t_token	**tail)
 {
-	int i = 0;
-	token_t *head;
-	token_t *tail;
-	token_t *token;
+	if (str[*i] == '|')
+	{
+		if (!push_op(head, tail, T_PIPE))
+			return (free_tokens_list(*head), -1);
+		(*i)++;
+		return (1);
+	}
+	if (str[*i] == '<' || str[*i] == '>')
+	{
+		if (str[*i] == '<' && str[*i + 1] == '<')
+		{
+			if (!push_op(head, tail, T_HEREDOC))
+				return (free_tokens_list(*head), -1);
+			*i += 2;
+			return (1);
+		}
+		if (str[*i] == '>' && str[*i + 1] == '>')
+		{
+			if (!push_op(head, tail, T_REDIR_APPEND))
+				return (free_tokens_list(*head), -1);
+			*i += 2;
+			return (1);
+		}
+		if (!redirect_choice(*head, *tail, str[*i]))
+			return (free_tokens_list(*head), -1);
+		(*i)++;
+		return (1);
+	}
+	return (0);
+}
 
+// purpose is to recognise WORD or quoted token starting at ni = new index
+// what this code do why free txt? what is txt?
+int	extract_quoted_word(const char *str, int *i, t_token	**head, t_token	**tail)
+{
+	token_t	*token;
+	int		ni;
+	char	*txt;
+
+	txt = NULL;
+	if (str[*i] == '\'' || str[*i] == '"')
+	{
+		ni = consume_quoted(str, *i, &txt);
+		if (ni < 0)
+			return (free_tokens_list(*head), -1); // unmatched quote
+		token = tok_new(T_WORD, txt, str[*i]);
+		if (!token)
+		{
+			free(txt);
+			return (free_tokens_list(*head), -1);
+		}
+		if (!head)
+			head = &token;
+		*tail = append_token(*tail, token);
+		*i = ni;
+		return (1);
+	}
+	return (0);
+}
+
+// unquoted word: run until space or operator
+// looks like we needd the start because it is before the word?
+// allow embedded quoted parts inside word
+// we will include the quotes' contents inline; to keep lexer simple,
+// just advance index
+// drop — parser will reconstruct properly; here we keep raw slice uh what?
+// simplify  needs
+int	extract_unquoted_word(const char *str, int *i, t_token	**head,
+		t_token	**tail)
+{
+	int		start;
+	char	*inner;
+	int		ni;
+	char	*txt;
+	token_t	*token;
+
+	start = *i;
+	txt = NULL;
+	token = NULL;
+	ni = 0;
+	while (str[*i] && !ft_isspace((unsigned char)str[*i]) && str[*i] != '|'
+		&& str[*i] != '<' && str[*i] != '>')
+	{
+		inner = NULL;
+		if (str[*i] == '\'' || str[*i] == '"')
+		{
+			ni = consume_quoted(str, *i, &inner);
+			if (ni < 0)
+				return (free(inner), free_tokens_list(*head), -1);
+			i = &ni;
+			free(inner);
+		}
+		else
+		{
+			if (str[*i] == '\\' && str[*i + 1])
+				*i += 2;
+			else
+				(*i)++;
+		}
+	}
+	txt = ft_strndup(str + start, *i - start);
+	if (!txt)
+		return (free_tokens_list(*head), -1);
+	token = tok_new(T_WORD, txt, 0);
+	if (!token)
+		return (free(txt), free_tokens_list(*head), -1);
+	if (*head == NULL)
+		*head = token;
+	*tail = append_token(*tail, token);
+	return (0);
+}
+// foreign extract unquoted
+/* Assumes:
+   - consume_quoted(const char *s, int idx,
+	char **out) returns index after closing quote (>=0) or -1 on error,
+	allocates *out on success.
+   - ft_isspace, ft_strndup, tok_new, append_token exist.
+   - t_token	is your token type.
+*/
+/*static int	extract_unquoted_word(const char *str, int *ip, t_token	**head,
+		t_token	**tail)
+{
+	int		start;
+	char	*inner;
+	char	*txt;
+	token_t	*token;
+	int		ni;
+
+	start = *ip;
+	inner = NULL;
+	txt = NULL;
+	while (str[*ip] && !ft_isspace((unsigned char)str[*ip]) && str[*ip] != '|'
+		&& str[*ip] != '<' && str[*ip] != '>')
+	{
+		if (str[*ip] == '\'' || str[*ip] == '"')
+		{
+			inner = NULL;
+			ni = consume_quoted(str, *ip, &inner); // ni >= 0 on success 
+			if (ni < 0)
+			{
+				free(inner);
+				return (-1); // caller handles overall cleanup 
+			}
+			free(inner);   // we only needed to skip quoted part here 
+			*ip = (int)ni; // advance past quoted part 
+		}
+		else
+		{
+			if (str[*ip] == '\\' && str[*ip + 1])
+				*ip += 2;
+			else
+				(*ip)++;
+		}
+	}
+	txt = ft_strndup(str + start, *ip - start);
+	if (!txt)
+		return (-1);
+	token = tok_new(T_WORD, txt, 0);
+	if (!token)
+	{
+		free(txt); // tok_new failed and didn't take ownership 
+		return (-1);
+	}
+	if (*head == NULL)
+		*head = token;
+	*tail = append_token(*tail, token); // append_token returns new tail 
+	return (0);
+}
+*/
+// skip escaped char
+// take substring start..i-1 doesn't make sense since there is no start i -1
+
+// the whole point of lexer is to give the type of the input and for now just remove spaces
+// but by return type we already can see it return a linked list of tokens
+token_t	*lexer(const char *str)
+{
+	int		i;
+	token_t	*head;
+	token_t	*tail;
+	int		res;
+	int		start;
+	/*	char	*txt;
+		int		ni;
+
+		token_t	*token;
+		char	*inner;*/
+	i = 0;
 	head = NULL;
 	tail = NULL;
 	while (str[i])
