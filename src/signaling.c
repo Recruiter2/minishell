6defcc4008c64cd143222ed8169238ef20402cb6
Tim <timour1712@gmail.com>
2026-06-20 17:22:34 +0200

    fix

--- src/signaling.c
+++ src/signaling.c
@@ -6,7 +6,7 @@
 /*   By: tzinaliy <tzinaliy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 16:29:56 by tzinaliy          #+#    #+#             */
-/*   Updated: 2026/06/05 12:18:38 by tzinaliy         ###   ########.fr       */
+/*   Updated: 2026/06/20 13:20:41 by tzinaliy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 	/* Write newline to move to new line */
 	/* Clear current readline buffer and show prompt again */
 
-void sigint_handler(int sig)
+void	sigint_handler(int sig)
 {
 	(void)sig;
 	write(1, "\n", 1);
-	rl_replace_line("", 0); 
+	rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
 }
