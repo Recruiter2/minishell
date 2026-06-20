24354e487765bbae63d068cd12c4ed92c7f0c759
Tim <timour1712@gmail.com>
2026-06-20 17:20:29 +0200

    fix to rqmts

--- src/history.c
+++ src/history.c
@@ -6,29 +6,30 @@
 /*   By: tzinaliy <tzinaliy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 14:55:56 by tzinaliy          #+#    #+#             */
-/*   Updated: 2026/06/06 16:03:43 by tzinaliy         ###   ########.fr       */
+/*   Updated: 2026/06/20 11:49:44 by tzinaliy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void add_shell_history(const char *line)
+void	add_shell_history(const char *line)
 {
 	if (!line || *line == '\0')
-		return;
+		return ;
 	add_history(line);
 }
 
 // Print history builtin: "history"
 // start index at 1 to match bash
-void builtin_history(void)
+void	builtin_history(void)
 {
-	int i;
+	int			i;
+	HIST_ENTRY	**hist_list;
 
 	i = 0;
-	HIST_ENTRY **hist_list = history_list();
+	hist_list = history_list();
 	if (!hist_list)
-		return;
+		return ;
 	while (hist_list[i] != NULL)
 	{
 		ft_printf("%d  %s\n", i + history_base, hist_list[i]->line);
