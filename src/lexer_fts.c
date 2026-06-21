038a0a9e2420fe9bc31890aa8b20512c8174c0a2
Tim <timour1712@gmail.com>
2026-06-22 00:28:16 +0200

    add comments; rm old

--- src/lexer_fts.c
+++ src/lexer_fts.c
@@ -6,7 +6,7 @@
 /*   By: tzinaliy <tzinaliy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/20 13:48:47 by tzinaliy          #+#    #+#             */
-/*   Updated: 2026/06/20 23:56:59 by tzinaliy         ###   ########.fr       */
+/*   Updated: 2026/06/21 23:27:01 by tzinaliy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 // purpose is to recognise WORD or quoted token starting at ni = new index
 // what this code do why free txt? what is txt?
-// unmatched quote if ni < 0
+// this code extracts a quoted word (aka "hi")
+// we free txt because it is memory allocated in consume quote via strdup
+// txt is the quoted extracted word (aka "hello")
+// code comment : unmatched quote if ni < 0
 int	extract_quoted_word(char *str, int *i, t_token **head, t_token **tail)
 {
 	t_token	*token;
@@ -103,47 +106,3 @@ int	get_unquoted_word(char *str, int *i, t_token **head, t_token **tail)
 	*tail = append_token(*tail, token);
 	return (0);
 }
-/*int	extract_unquoted_word(char *str, int *i, t_token
-	**head, t_token	**tail)
-{
-	int		start;
-	char	*inner;
-	int		ni;
-	char	*txt;
-	t_token	*token;
-
-	start = *i;
-	txt = NULL;
-	token = NULL;
-	ni = 0;
-	while (str[*i] && !ft_isspace((unsigned char)str[*i]) && str[*i] != '|'
-		&& str[*i] != '<' && str[*i] != '>')
-	{
-		inner = NULL;
-		if (str[*i] == '\'' || str[*i] == '"')
-		{
-			ni = consume_quoted(str, *i, &inner);
-			if (ni < 0)
-				return (free(inner), free_tokens_list(*head), -1);
-			i = &ni;
-			free(inner);
-		}
-		else
-		{
-			if (str[*i] == '\\' && str[*i + 1])
-				*i += 2;
-			else
-				(*i)++;
-		}
-	}
-	txt = ft_strndup(str + start, *i - start);
-	if (!txt)
-		return (free_tokens_list(*head), -1);
-	token = tok_new(T_WORD, txt, 0);
-	if (!token)
-		return (free(txt), free_tokens_list(*head), -1);
-	if (*head == NULL)
-		*head = token;
-	*tail = append_token(*tail, token);
-	return (0);
-}*/
