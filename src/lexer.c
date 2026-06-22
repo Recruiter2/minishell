c398576d6f7ef65cb30c2daefcefb51402cbf50c
Tim <timour1712@gmail.com>
2026-06-22 09:08:50 +0200

    move ft

--- src/lexer.c
+++ src/lexer.c
@@ -88,36 +88,6 @@ void	free_tokens_list(t_token	*head)
 }
 // free the token struct allocated in tok_new
 
-// create and append an operator token; returns 0 on OOM
-int	push_op(t_token	**head, t_token	**tail, token_type type)
-{
-	t_token	*token;
-
-	token = tok_new(type, NULL, 0);
-	if (!token)
-		return (0);
-	if (!*head)
-		*head = token;
-	*tail = append_token(*tail, token);
-	return (1);
-}
-
-int	ft_isspace_pp(char c, int *i)
-{
-	if (ft_isspace(c))
-	{
-		(*i)++;
-		return (0);
-	}
-	return (1);
-}
-
-void	init_lex(int *i, t_token **head, t_token **tail)
-{
-	*i = 0;
-	*head = NULL;
-	*tail = NULL;
-}
 // foreign extract unquoted
 /* Assumes:
 	- consume_quoted(const char *s, int idx,
