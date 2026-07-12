6f0871ca661f028a7543069efb88c72af74cc1ce
Tim <timour1712@gmail.com>
2026-07-13 00:52:25 +0200

    rm list since they are imported in executor too so to avoid conflict? could be fatal tho

--- Libft/libft.h
+++ Libft/libft.h
@@ -22,12 +22,12 @@
 # include <limits.h>
 # include "ft_printf/ft_printf.h"
 
-typedef struct s_list
+/*typedef struct s_list // comment out because of duplicate conflicting...
 {
 	void			*content;
 	struct s_list	*next;
 }					t_list;
-
+//*/
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
@@ -83,7 +83,7 @@ void	ft_putendl_fd(char *str, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
 //list handling
-t_list	*ft_lstnew(void *content);
+/*t_list	*ft_lstnew(void *content); //commented out because of comment above
 void	ft_lstadd_front(t_list **lst, t_list *new_);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
@@ -92,7 +92,7 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-
+//*/
 /*gnl*/
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 7
