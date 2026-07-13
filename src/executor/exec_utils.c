1b0cbd941713991799cb8f4732d795108585b9fb
Tim <timour1712@gmail.com>
2026-07-13 14:13:54 +0200

    fix error in compiler of unused variable

--- src/executor/exec_utils.c
+++ src/executor/exec_utils.c
@@ -15,16 +15,12 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	size_t	i;
-
-	i = 0;
 	while (*s1 || *s2)
 	{
 		if (*s1 - *s2)
 			return (*(unsigned char *)s1 - *(unsigned char *)s2);
 		s1++;
 		s2++;
-		i++;
 	}
 	return (0);
 }
