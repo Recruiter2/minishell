fdb3fff58a341e52825b136a58dd2a84f1075f72
MarioX1 <falcino@gmail.com>
2026-08-03 21:52:54 +0200

    norm last file in executor

--- src/executor/path.c
+++ src/executor/path.c
@@ -6,7 +6,7 @@
 /*   By: marhuber <marhuber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 13:48:13 by marhuber          #+#    #+#             */
-/*   Updated: 2026/08/03 18:22:34 by marhuber         ###   ########.fr       */
+/*   Updated: 2026/08/03 21:52:21 by marhuber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,14 @@ int			ft_strncmp(const char *s1, const char *s2, size_t n);
 char		*join_str_c_str(char const *s1, char c, char const *s2);
 void		put_str_fd(const char *s, int fd);
 
+/*
+This function deletes first any existing collection of strings with the PATH.
+Then it searches among the environment variables (in ther string format) for 
+	the variable PATH, presuming it might contain updated data.
+If found, the fonction extracts from the value of PATH its elements, which are 
+	separated by ':'-chars.
+It saves the new elements of PATH as a collection of strings.
+*/
 int	extract_path(t_ctx *ctx)
 {
 	char	**it;
@@ -45,13 +53,22 @@ int	extract_path(t_ctx *ctx)
 	return (0);
 }
 
-/**
-cf section "3.7.2 Command Search and Execution" in bash manual
+/*
+Cf section "3.7.2 Command Search and Execution" in bash manual
+This fonction tries to find a simple command (e.g. without args) in the PATH.
+It tries to access an executable with the name searched for in each folder 
+	in the PATH.
+If it finds one, then substitutes *argv (e.g. argv[0]) with the absolute path of 
+	said executable.
+If the search is unsuccesfull (or if the PATH collection of strings is 
+	unavailable), it outputs an error.
 */
 int	find_cmd(char **path, char **argv)
 {
 	char	*tmp;
+
 	if (path)
+	{
 		while (*path)
 		{
 			tmp = join_str_c_str(*path, '/', *argv);
@@ -69,8 +86,8 @@ int	find_cmd(char **path, char **argv)
 				return (0);
 			}
 		}
+	}
 	put_str_fd("minishell: command not found: ", 2);
 	put_str_fd(*argv, 2);
-	put_str_fd("\n", 2);
-	return (127);
+	return (put_str_fd("\n", 2), 127);
 }
